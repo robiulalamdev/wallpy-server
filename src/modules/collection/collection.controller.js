35aeffe7f528e1826f961c73d8e7f57d1e25c669
robiulalamdev <robiulalam762400@gmail.com>
2024-04-04 00:11:07 +0600

    collection add remove for wallpaper done

--- src/modules/collection/collection.controller.js
+++ src/modules/collection/collection.controller.js
@@ -60,18 +60,28 @@ const getCollectionsList = async (req, res) => {
   try {
     const isExistUser = await getUserById(req.params.userId);
     if (isExistUser) {
-      const result = await Collection.find({
+      let result = await Collection.find({
         user: isExistUser?._id?.toString(),
         status: true,
       })
         .sort({ _id: -1 })
-        .select("name");
+        .select("name wallpapers");
 
+      let isExist = false;
+      for (let i = 0; i < result.length; i++) {
+        const isIndex = await result[i]?.wallpapers?.findIndex(
+          (nItem) => nItem?.toString() === req.params?.wallpaperId
+        );
+        if (isIndex !== -1) {
+          result[i] = { ...result[i].toObject(), added: true };
+          isExist = true;
+        }
+      }
       res.status(200).json({
         status: 200,
         success: true,
         message: "Collections Retrieve Success",
-        data: result,
+        data: { isExist: isExist, data: result },
       });
     } else {
       res.status(404).json({
@@ -158,6 +168,7 @@ const removeMyCollections = async (req, res) => {
     });
   }
 };
+
 const updateMyCollections = async (req, res) => {
   try {
     const isExistUser = await getUserById(req.user._id);
@@ -193,6 +204,66 @@ const updateMyCollections = async (req, res) => {
   }
 };
 
+const addRemoveCollectionItem = async (req, res) => {
+  try {
+    const isExistUser = await getUserById(req.user._id);
+    if (isExistUser) {
+      const existingDocument = await Collection.findOne({
+        _id: req.params?.collectionId,
+        wallpapers: req.body.wallpaperId,
+      });
+      if (existingDocument) {
+        const result = await Collection.findByIdAndUpdate(
+          { _id: req.params?.collectionId },
+          {
+            $pull: { wallpapers: req.body.wallpaperId },
+          },
+          { new: false }
+        );
+        res.status(200).json({
+          status: 200,
+          success: true,
+          message: "Collections Update Success",
+          data: result,
+        });
+      } else {
+        const result = await Collection.findByIdAndUpdate(
+          { _id: req.params?.collectionId },
+          {
+            $push: {
+              wallpapers: {
+                $each: [req.body.wallpaperId],
+                $position: 0,
+              },
+            },
+          },
+          { new: false }
+        );
+        res.status(200).json({
+          status: 200,
+          success: true,
+          message: "Collections Update Success",
+          data: result,
+        });
+      }
+    } else {
+      res.status(404).json({
+        status: 404,
+        success: false,
+        type: "email",
+        message: "User Not Found!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: 500,
+      success: false,
+      message: "Internal Server Error",
+      error_message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createCollection,
   getMyCollections,
@@ -200,4 +271,5 @@ module.exports = {
   removeMyCollections,
   updateMyCollections,
   getCollectionsList,
+  addRemoveCollectionItem,
 };
--- src/modules/collection/collection.route.js
+++ src/modules/collection/collection.route.js
@@ -7,14 +7,20 @@ const {
   removeMyCollections,
   getMyProfileCollections,
   getCollectionsList,
+  addRemoveCollectionItem,
 } = require("./collection.controller");
 const router = express.Router();
 
 router.post("/create", isAuth, createCollection);
 router.get("/my-collections", isAuth, getMyCollections);
 router.get("/my-profile-collections/:userId", getMyProfileCollections);
-router.get("/list/:userId", getCollectionsList);
+router.get("/list/:userId/:wallpaperId", getCollectionsList);
 router.delete("/remove-my-collections", isAuth, removeMyCollections);
 router.patch("/update-my-collections", isAuth, updateMyCollections);
+router.post(
+  "/add-remove-wallpaper/:collectionId",
+  isAuth,
+  addRemoveCollectionItem
+);
 
 module.exports = { collectionRoutes: router };
