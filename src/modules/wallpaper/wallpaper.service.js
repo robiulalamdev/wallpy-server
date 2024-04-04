f07b0320dd9adc152cb23e6169a9b5e8e4540a39
robiulalamdev <robiulalam762400@gmail.com>
2024-04-04 13:51:40 +0600

    update wallpaper slug making

--- src/modules/wallpaper/wallpaper.service.js
+++ src/modules/wallpaper/wallpaper.service.js
@@ -5,18 +5,56 @@ const wallpapersMake = async (files, userId) => {
     if (url) {
       let lastDotIndex = url.lastIndexOf(".");
       if (lastDotIndex !== -1) {
-        let urlWithoutExtension = url.substring(0, lastDotIndex).split("\\");
-        const slug = urlWithoutExtension[urlWithoutExtension.length - 1];
-        wallpapers.push({
-          user: userId,
-          wallpaper: files[i]?.path,
-          slug: slug,
-        });
+        let firstSplit = url.substring(0, lastDotIndex).split("\\");
+        if (firstSplit?.length > 1) {
+          const slug = firstSplit[firstSplit.length - 1];
+          wallpapers.push({
+            user: userId,
+            wallpaper: files[i]?.path,
+            slug: slug,
+          });
+        } else if (url.substring(0, lastDotIndex).split("//")?.length > 2) {
+          const newSplit = url.substring(0, lastDotIndex).split("//");
+          const slug = newSplit[newSplit.length - 1];
+          wallpapers.push({
+            user: userId,
+            wallpaper: files[i]?.path,
+            slug: slug,
+          });
+        } else {
+          const newSplit = url.substring(0, lastDotIndex).split("/");
+          const slug = newSplit[newSplit.length - 1];
+          wallpapers.push({
+            user: userId,
+            wallpaper: files[i]?.path,
+            slug: slug,
+          });
+        }
       }
     }
   }
   return wallpapers;
 };
+
+// const wallpapersMake = async (files, userId) => {
+//   const wallpapers = [];
+//   for (let i = 0; i < files.length; i++) {
+//     const url = files[i]?.path;
+//     if (url) {
+//       let lastDotIndex = url.lastIndexOf(".");
+//       if (lastDotIndex !== -1) {
+//         let urlWithoutExtension = url.substring(0, lastDotIndex).split("\\");
+//         const slug = urlWithoutExtension[urlWithoutExtension.length - 1];
+//         wallpapers.push({
+//           user: userId,
+//           wallpaper: files[i]?.path,
+//           slug: slug,
+//         });
+//       }
+//     }
+//   }
+//   return wallpapers;
+// };
 
 module.exports = {
   wallpapersMake,
