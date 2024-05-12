177fb7ff79fa318c35e78c04ca59c5c8fd063c87
robiulalamdev <robiulalam762400@gmail.com>
2024-05-12 23:56:49 +0600

    cors origin removed

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ const app = express();
 const path = require("path");
 
 // middleware
-app.use(
-  cors({
-    origin: [VARIABLES.CLIENT_URL, VARIABLES.LOCAL_URL],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [VARIABLES.CLIENT_URL, VARIABLES.LOCAL_URL],
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(express.json({ limit: "500mb" }));
 app.use(
   express.urlencoded({ limit: "500mb", extended: true, parameterLimit: 500000 })
