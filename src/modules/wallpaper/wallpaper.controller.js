3192444ee53138c4bd4a620d8b16d4b25945202d
robiulalamdev <robiulalam762400@gmail.com>
2024-07-06 22:11:05 +0600

    version: 2.2.3

--- src/modules/wallpaper/wallpaper.controller.js
+++ src/modules/wallpaper/wallpaper.controller.js
@@ -528,7 +528,7 @@ const getFeaturedWallpapers = async (req, res) => {
   try {
     const result = await Wallpaper.aggregate([
       { $match: { status: WALLPAPER_ENUMS.STATUS[1], isFeatured: true } },
-      { $sample: { size: 6 } },
+      { $sample: { size: 3 } },
     ]);
 
     res.status(200).json({
