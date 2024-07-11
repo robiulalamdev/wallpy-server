42ebfa3c459f9cf1aeebca6211d9a5b1b37e0107
robiulalamdev <robiulalam762400@gmail.com>
2024-07-11 12:17:16 +0600

    featured conteroller update

--- src/modules/featured/featured.controller.js
+++ src/modules/featured/featured.controller.js
@@ -2,7 +2,8 @@ const Featured = require("./featured.model");
 
 const addFeatured = async (req, res) => {
   try {
-    await Featured.deleteMany({ type: "Contact" });
+    const type = req.body?.type || "";
+    await Featured.deleteMany({ type: type });
     const result = await Featured.insertMany(req.body?.items);
     res.status(200).json({
       status: 200,
@@ -59,8 +60,90 @@ const getContactFeatured = async (req, res) => {
     });
   }
 };
+const getStaffFeatured = async (req, res) => {
+  try {
+    const result = await Featured.find({
+      type: "Staff",
+      targetType: "Wallpaper",
+    })
+      .limit(6)
+      .sort({ _id: -1 })
+      .then(async function (items) {
+        const populatedFeatured = await Promise.all(
+          items.map(async (currentItem) => {
+            await currentItem.populate({
+              path: "targetId",
+              model: currentItem.targetType,
+              select: "wallpaper slug",
+            });
+            return {
+              ...currentItem.toObject(),
+              wallpaper: currentItem?.targetId?.wallpaper,
+              slug: currentItem?.targetId?.slug,
+              targetId: currentItem?.targetId?._id,
+            };
+          })
+        );
+        return populatedFeatured;
+      });
+
+    res.status(201).json({
+      success: true,
+      message: "Featured Retrieve Success",
+      data: result,
+    });
+  } catch (error) {
+    res.status(201).json({
+      success: false,
+      message: "Featured Retrieve Failed",
+      error_message: error.message,
+    });
+  }
+};
+
+const getFeaturedData = async (req, res) => {
+  try {
+    const { type = "", targetType = "", limit } = req.query;
+
+    const items = await Featured.aggregate([
+      { $match: { type: type, targetType: targetType } },
+      { $sample: { size: parseInt(limit) || 3 } },
+    ]);
+
+    const populatedFeatured = await Promise.all(
+      items.map(async (currentItem) => {
+        const populatedItem = await Featured.populate(currentItem, {
+          path: "targetId",
+          model: currentItem.targetType,
+          select: "wallpaper slug",
+        });
+        return {
+          ...populatedItem,
+          wallpaper: populatedItem?.targetId?.wallpaper,
+          slug: populatedItem?.targetId?.slug,
+          targetId: populatedItem?.targetId?._id,
+        };
+      })
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Featured Retrieve Success",
+      data: populatedFeatured,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 500,
+      success: false,
+      message: "Internal Server Error",
+      error_message: error.message,
+    });
+  }
+};
 
 module.exports = {
   addFeatured,
   getContactFeatured,
+  getStaffFeatured,
+  getFeaturedData,
 };
--- src/modules/featured/featured.route.js
+++ src/modules/featured/featured.route.js
@@ -6,7 +6,12 @@ const {
 } = require("../../middlewares/auth");
 const { upload, handleMulterError } = require("../../config/multer");
 const { ROLE_DATA } = require("../user/user.constants");
-const { addFeatured, getContactFeatured } = require("./featured.controller");
+const {
+  addFeatured,
+  getContactFeatured,
+  getFeaturedData,
+  getStaffFeatured,
+} = require("./featured.controller");
 const router = express.Router();
 
 router.post(
@@ -14,10 +19,17 @@ router.post(
   isAuthenticated([ROLE_DATA.ADMIN, ROLE_DATA.MOD]),
   addFeatured
 );
+router.get("/", getFeaturedData);
+
 router.get(
   "/contact",
-  // isAuthenticated([ROLE_DATA.ADMIN, ROLE_DATA.MOD]),
+  isAuthenticated([ROLE_DATA.ADMIN, ROLE_DATA.MOD]),
   getContactFeatured
+);
+router.get(
+  "/staff",
+  isAuthenticated([ROLE_DATA.ADMIN, ROLE_DATA.MOD]),
+  getStaffFeatured
 );
 
 module.exports = { featuredRoutes: router };
